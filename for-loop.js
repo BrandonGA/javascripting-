d3fddfc4e941d9c6696b3d04289a42a9fd5087c6
Brandon Galea-Andrews <brandon.galeaandrews@gmail.com>
2017-02-20 23:57:08 +0000

    Initial commit with all my work so far

---
+++ for-loop.js
@@ -0,0 +1,7 @@
+
+var total = 0
+var limit = 10
+
+for ( var total = 0; total < limit.length; total += i) {
+}
+console.log(total)
---
+++ looping-through-arrays.js
@@ -0,0 +1,6 @@
+var pets = ['cat', 'dog', 'rat'];
+for (pets[i] = pets[i] + 's'); {
+
+}
+
+console.log(pets)
